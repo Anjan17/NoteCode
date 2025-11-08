e62f9e311189ff98f620d0dc8fff93b04e422cbf
Anjan Kumar Behera <anjan.behera@leadsquared.com>
2025-11-08 13:11:02 +0530

    chore: update prebuild script

--- backend/pre-build.sh
+++ backend/pre-build.sh
@@ -2,4 +2,4 @@ echo "Node $(node -v), npm $(npm -v)"
 npm config get registry
 npm config list --location=project
 npm ping || true
-[ -z "$WEB_APP_TOKEN" ] && echo "WEB_APP_TOKEN is EMPTY" || echo "WEB_APP_TOKEN is SET"
+[ -z "$WEB_APP_TOKEN_1" ] && echo "WEB_APP_TOKEN_1 is EMPTY" || echo "WEB_APP_TOKEN_1 is SET"
